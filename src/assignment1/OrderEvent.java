20825a25e0005a069ac4423b30eb81cd95a853d2
Brendan Edmonds <brendan.edmonds@carpetcall.com.au>
2022-08-01 16:26:26 +1000

    changed leave time in order events

--- src/assignment1/OrderEvent.java
+++ src/assignment1/OrderEvent.java
@@ -14,9 +14,9 @@ public class OrderEvent extends Event {
 
     @Override
     public void process(ShopModel shopModel, IScheduler scheduler) {
-        shopModel.serveOrder(group.getArrivalTime() + 1, group);
+        shopModel.serveOrder(this.getTime(), group);
         //Leave event
-        Event leaveEvent = new LeaveEvent(group, group.getArrivalTime() + 11);
+        Event leaveEvent = new LeaveEvent(group, this.getTime()+10);
         scheduler.schedule(leaveEvent);
     }
 
